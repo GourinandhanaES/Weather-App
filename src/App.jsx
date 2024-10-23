ab6022b383f91924e9c58aea98a5e96320c7aeb4
GourinandhanaES <gourinandhana028@gmail.com>
2024-10-24 03:32:44 +0530

    Updated the registration form and added new features

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import './App.css';
+import sunnyImage from './assets/sunny.png';
+import cloudyImage from './assets/cloudy.png';
+import rainyImage from './assets/rainy.png';
+import thunderstormImage from './assets/thunder.png';
+import defaultImage from './assets/default.png'; 
 
 const App = () => {
   const [weather, setWeather] = useState(null);
   const [city, setCity] = useState('');
   const [placeholder, setPlaceholder] = useState('Enter the place');
+  const [weatherImage, setWeatherImage] = useState('');
 
   useEffect(() => {
     let index = 0;
@@ -23,6 +29,18 @@ const App = () => {
     try {
       const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
       setWeather(response.data);
+      const weatherCondition = response.data.weather[0].main.toLowerCase();
+      if (weatherCondition.includes('clear')) {
+        setWeatherImage(sunnyImage);
+      } else if (weatherCondition.includes('clouds')) {
+        setWeatherImage(cloudyImage);
+      } else if (weatherCondition.includes('rain')) {
+        setWeatherImage(rainyImage);
+      } else if (weatherCondition.includes('thunderstorm')) {
+        setWeatherImage(thunderstormImage);
+      } else {
+        setWeatherImage(defaultImage);
+      }
     } catch (error) {
       console.error('Error fetching the weather data:', error);
     }
@@ -41,9 +59,14 @@ const App = () => {
 
       {weather && (
         <div className="card">
-          <h2>{weather.name}</h2>
-          <p>{weather.main.temp}°C</p>
-          <p>{weather.weather[0].description}</p>
+          <div className="weather-info">
+            <h2>{weather.name}</h2>
+            <p>{weather.main.temp}°C</p>
+            <p>{weather.weather[0].description}</p>
+          </div>
+          <div className="weather-image">
+            <img src={weatherImage} alt={weather.weather[0].main}  />
+          </div>
         </div>
       )}
     </div>
